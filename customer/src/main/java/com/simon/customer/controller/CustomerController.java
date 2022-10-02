40e9eb239d3c31d6f6bfb308ba41b04fc4546e46
simonsimon9 <simonkuang9@outlook.com>
2022-10-02 16:54:14 -0400

    refactored records to class in customer package

--- customer/src/main/java/com/simon/customer/controller/CustomerController.java
+++ customer/src/main/java/com/simon/customer/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.simon.customer.controller;
 import com.simon.customer.model.CustomerRegistrationRequest;
 import com.simon.customer.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController(CustomerService customerService) {
-
+public class CustomerController {
+    private final CustomerService customerService;
+    @Autowired
+    CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequestRequest){
         log.info("new customer registration {}", customerRegistrationRequestRequest);
--- customer/src/main/java/com/simon/customer/service/CustomerService.java
+++ customer/src/main/java/com/simon/customer/service/CustomerService.java
@@ -3,10 +3,16 @@ package com.simon.customer.service;
 import com.simon.customer.model.Customer;
 import com.simon.customer.model.CustomerRegistrationRequest;
 import com.simon.customer.repo.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public class CustomerService{
+    private final CustomerRepository customerRepository;
+    @Autowired
+    CustomerService(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder()
                 .firstName(request.firstName())

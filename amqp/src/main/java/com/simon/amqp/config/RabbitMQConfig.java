3e9f1a3a65ed288afa939f6ffe0b1b84fba72052
simonsimon9 <simonkuang9@outlook.com>
2022-10-06 12:58:02 -0400

    added rabbit listener

--- amqp/src/main/java/com/simon/amqp/config/RabbitMQConfig.java
+++ amqp/src/main/java/com/simon/amqp/config/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.simon.amqp.config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -17,16 +18,24 @@ public class RabbitMQConfig {
 
     @Bean
     public AmqpTemplate amqpTemplate(){
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-
+        RabbitTemplate rabbitTemplate =
+                new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jacksonConverter());  //sends as json
-
         return rabbitTemplate;
     }
 
     @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(){
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+    @Bean
     public MessageConverter jacksonConverter(){
-        MessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        MessageConverter jackson2JsonMessageConverter =
+                new Jackson2JsonMessageConverter();
         return jackson2JsonMessageConverter;
     }
 }

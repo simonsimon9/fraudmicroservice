3b6212228e56fc476c934ed45dc91915335ae98d
simonsimon9 <simonkuang9@outlook.com>
2022-10-06 19:13:21 -0400

    customer ms publishing messages to queue

--- customer/src/main/java/com/simon/customer/CustomerApplication.java
+++ customer/src/main/java/com/simon/customer/CustomerApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages =
+                {"com.simon.customer",
+                        "com.simon.amqp"}
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.simon.clients"
